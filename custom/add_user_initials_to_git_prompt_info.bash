170676f806ac065da4df9f413d99d0b1d25ccc65
Steve Gravrock <sgravrock@pivotal.io>
2017-06-09 11:10:33 -0700

    Bash prompt shows pending merge or rebase conflicts

--- custom/add_user_initials_to_git_prompt_info.bash
+++ custom/add_user_initials_to_git_prompt_info.bash
@@ -4,5 +4,47 @@ function git_prompt_info {
   GIT_DUET=$(echo $(git config --get-regexp ^duet.env.git-.*-name | sed -e 's/^.*-name //' | tr 'A-Z' 'a-z' | sed -e 's/\([a-z]\)[^ +]*./\1/g' ) | sed -e 's/ /+/')
   GIT_PAIR=${GIT_DUET:-`git config user.initials | sed 's% %+%'`}
   GIT_INITIALS=${GIT_TOGETHER:-$GIT_PAIR}
-  echo -e " $GIT_INITIALS$SCM_PREFIX$SCM_BRANCH$SCM_STATE$SCM_SUFFIX"
+  gitdir=$(git_dir)
+
+  if is_merging "$gitdir"; then
+    echo -e " \e[31mMERGING\e[0m"
+  elif is_rebasing "$gitdir"; then
+    echo -e " \e[31mREBASING\e[0m"
+  else
+    echo -e " $GIT_INITIALS$SCM_PREFIX$SCM_BRANCH$SCM_STATE$SCM_SUFFIX"
+  fi
+}
+
+function git_dir {
+  (
+    while [ "$(pwd)" != / ]; do
+      if [ -d .git ]; then
+        pwd
+        exit
+      fi
+      cd ..
+    done
+  )
+}
+
+function is_merging {
+  gd="$1"
+
+  if [ -z "$gd" ]; then
+    return 1
+  fi
+
+  [ -f "${gd}/.git/MERGE_HEAD" ]
+  return $?
+}
+
+function is_rebasing {
+  gd="$1"
+
+  if [ -z "$gd" ]; then
+    return 1
+  fi
+
+  [ -d "${gd}/.git/rebase-apply" ] || [ -d "${gd}/.git/rebase-merge" ]
+  return $?
 }
